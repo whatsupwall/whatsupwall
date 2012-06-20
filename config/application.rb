88f77a40616eb4622b3282ebf493f28c5e67937c
Thomas Roger <throger@gmail.com>
2012-06-21 00:35:42 +0200

    Add js and css files to precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,8 @@ module Whatsupwall
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += ['*.js', '*.css']
+
     config.generators do |g|
       g.view_specs false
       g.helper_specs false
